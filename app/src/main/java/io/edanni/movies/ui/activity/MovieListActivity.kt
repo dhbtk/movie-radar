f6fb37b9471ce4e3bbcd1663deb32c0b86c98099
Eduardo Niehues <eduardo@edanni.io>
2017-11-20 03:36:14 -0200

    Initial movie list (untested)

--- app/src/main/java/io/edanni/movies/ui/activity/MovieListActivity.kt
+++ app/src/main/java/io/edanni/movies/ui/activity/MovieListActivity.kt
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity
 import io.edanni.movies.Application
 import io.edanni.movies.R
 import io.edanni.movies.domain.service.MovieService
+import io.edanni.movies.ui.adapter.MovieListAdapter
+import kotlinx.android.synthetic.main.activity_movie_list.*
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 import javax.inject.Inject
 
 class MovieListActivity : AppCompatActivity() {
@@ -13,9 +17,18 @@ class MovieListActivity : AppCompatActivity() {
     @Inject
     lateinit var movieService: MovieService
 
+    lateinit var movieListAdapter: MovieListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_list)
         Application.injector.inject(this)
+        movieListAdapter = MovieListAdapter(this)
+        this.gridView.adapter = movieListAdapter
+
+        movieService.getUpcomingMovies(1, "")
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ movieListAdapter.movies = it.results })
     }
 }
---
+++ app/src/main/java/io/edanni/movies/ui/adapter/MovieListAdapter.kt
@@ -0,0 +1,35 @@
+package io.edanni.movies.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.FrameLayout
+import io.edanni.movies.R
+import io.edanni.movies.infrastructure.api.dto.Movie
+import io.edanni.movies.ui.binder.MovieListBinder
+
+/**
+ * Created by eduardo on 20/11/2017.
+ */
+class MovieListAdapter(private val context: Context) : BaseAdapter() {
+    var movies: List<Movie> = emptyList()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val layout: FrameLayout = convertView as FrameLayout? ?: (inflater.inflate(R.layout.movie_list_item, parent) as FrameLayout)
+        MovieListBinder.bind(movies[position], layout, context)
+        return layout
+    }
+
+    override fun getItem(i: Int): Any = movies[i]
+
+    override fun getItemId(i: Int): Long = i.toLong()
+
+    override fun getCount(): Int = movies.size
+}
